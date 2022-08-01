87d9619796bf02458f48ec9e7006aa71a25778ee
rampg23 <99385323+rampg23@users.noreply.github.com>
2022-08-01 15:06:22 +0530

    Update WalletTest.java

--- src/test/java/com/thoughtworks/Wallet/WalletTest.java
+++ src/test/java/com/thoughtworks/Wallet/WalletTest.java
@@ -6,7 +6,8 @@ import static com.thoughtworks.Wallet.Currency.RUPEE;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-
+ramprakash.pg@thoughtworks.com
+hp_dvQ5crSjmUivViWW5RldHSJTHeO123owasaS%das
 public class WalletTest {
     @Test
     void shouldAddMoneyToWalletWhenAmountIsGiven() {
